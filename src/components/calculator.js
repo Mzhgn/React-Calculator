92660cdcead3b3c7a1afb3667cdbd5a2957c75cd
Mozhgan <mojganshah@gmail.com>
2025-02-15 19:25:05 +0330

    adding value variable

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 
 const calculator = () => {
+  const [value, setValue] = useState(" ");
+
   return (
     <div>
       <h1>Calculator</h1>
+      <div>{value}</div>
     </div>
   );
 };
