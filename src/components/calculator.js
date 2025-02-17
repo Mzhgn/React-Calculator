1cd48ff9c7f78560ef93d75bf553d59c6ebafeb8
Mozhgan <mojganshah@gmail.com>
2025-02-17 22:45:58 +0330

    updating the calculator function

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,6 +5,11 @@ import Display from "./Display";
 const calculator = () => {
   const [value, setValue] = useState(" ");
 
+  const handleButtonClick = (val) => {
+    if (val === "=") {
+    }
+  };
+
   return (
     <div>
       <h1>Calculator</h1>
