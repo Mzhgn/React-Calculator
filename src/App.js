e91f88d011575790a541ef57136edea5d960cf86
Mozhgan <mojganshah@gmail.com>
2025-02-26 21:50:41 +0330

    screen component added to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
 
 function App() {
   return (
     <div>
       <Wrapper>
-        <h1>Hello</h1>
+        <Screen>
+          <h1>Hello</h1>
+        </Screen>
       </Wrapper>
     </div>
   );
