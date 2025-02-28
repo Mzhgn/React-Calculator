d3ab89b636e1303dbb1996cde5682f79fec2dc8f
Mozhgan <mojganshah@gmail.com>
2025-02-28 21:51:59 +0330

    button components created

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Button = ({ value, onClick }) => {
-  <button onClick={onClick}>{value}</button>;
+const Button = () => {
+  return <div>Button</div>;
 };
 
 export default Button;
