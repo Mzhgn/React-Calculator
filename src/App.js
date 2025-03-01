73bfd20f9f73434e1c4767796316c51b6bccf097
Mozhgan <mojganshah@gmail.com>
2025-03-01 21:26:05 +0330

    button box added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
 
 function App() {
   return (
     <div>
       <Wrapper>
         <Screen />
+        <ButtonBox></ButtonBox>
 
         <h1>Hello</h1>
       </Wrapper>
