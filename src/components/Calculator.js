21af4c6e16a708c6359251fe708b84d8038ffe26
Mozhgan <mojganshah@gmail.com>
2025-03-18 23:03:18 +0330

    adding icons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,6 +13,19 @@ const Calculator = () => {
       </div>
       <div className="buttons">
         <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
+        <button>*</button>
+        <button>C</button>
+        <button>=</button>
+        <button>/</button>
       </div>
     </div>
   );
@@ -23,3 +36,4 @@ const handleClick = (value) => {
 const handleClear = () => {
   setInput(" ");
 };
+export default Calculator;
