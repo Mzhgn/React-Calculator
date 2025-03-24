9e1b483a792863ce00516035b6e3b796d5aa1fb2
Mozhgan <mojganshah@gmail.com>
2025-03-24 23:46:05 +0330

    modifying App

--- src/App.js
+++ src/App.js
@@ -2,4 +2,13 @@ import React from "react";
 import Calculator from "./components/Calculator";
 import "./App.css";
 
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React Calculator </h1>
+      <Calculator />
+    </div>
+  );
+};
+
 export default App;
