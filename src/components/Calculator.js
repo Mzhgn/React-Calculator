090975d2e3d9cb544f53cd569452a4290c4b1374
Mozhgan <mojganshah@gmail.com>
2025-04-07 23:41:02 +0330

    adding number array

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,6 +14,7 @@ const Calculator = () => {
     } catch (error) {
       setInput("Error");
     }
+    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   };
   return (
     <div className="calculator">
